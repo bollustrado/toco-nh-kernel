b1de89d2bb227b5cf648cea7fd18a0024d6addd5
root <root@nova>
2021-12-31 00:04:03 +0000

    add submodule extra_tools/drivers/rtl8814au

--- build1.sh
+++ build1.sh
@@ -3,11 +3,10 @@
 export ARCH=arm64
 export SUBARCH=arm64
 export DTC_EXT=dtc
-export PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}"
+#export PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}"
 export CC=clang
-export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-export KBUILD_COMPILER_STRING="$($HOME/dev/clang-14/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+#export KBUILD_COMPILER_STRING="$($HOME/dev/clang-14/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 
 COMPRESSION="gz"
 RELEASE="r1"
@@ -17,7 +16,7 @@ THREAD_COUNT=$(($(nproc --all)-0)) # Keep minus 1 for responsive desktop experie
 echo --CLEAN OUT
 echo - cleaning out
 rm -rf out
-#make O=out ARCH=arm64 mrproper
+#make O=out CC=clang ARCH=arm64 mrproper
 echo - cleaning generated Image
 rm _anykernel/Image.$COMPRESSION-dtb
 rm _anykernel/dtbo.img
@@ -28,21 +27,20 @@ echo - generate out directory
 mkdir out
 
 echo DEFCONFIG
-PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
-make  O=out ARCH=arm64 toco_nh3_defconfig
+#PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
+make  O=out CC=clang ARCH=arm64 toco_nh3_defconfig
 #make  O=out ARCH=arm64 toco_defconfig
 
 echo MENUCONFIG
-PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
-make O=out menuconfig
+#PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
+make O=out CC=clang menuconfig
 #make O=out xconfig
-PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
-make O=out savedefconfig
-PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
-make -j$THREAD_COUNT O=out \
+#PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
+make O=out CC=clang savedefconfig
+#PATH="$HOME/dev/clang-14/bin:$HOME/dev/aarch64-linux-gnu/bin:${PATH}" \
+make -j16 O=out \
                       ARCH=arm64 \
                       CC=clang \
-                      CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                       NM=llvm-nm \
                       OBJCOPY=llvm-objcopy \
@@ -51,6 +49,31 @@ make -j$THREAD_COUNT O=out \
 		      USE_CCACHE=1 \
 		      CCACHE_DIR=~/.ccache \
                       LD=ld.lld | tee kernel.log
+
+make modules_install -j16 O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip \
+                      USE_CCACHE=1 \
+                      CCACHE_DIR=~/.ccache \
+                      LD=ld.lld
+
+make headers_install -j16 O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      NM=llvm-nm \
+                      OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip \
+                      USE_CCACHE=1 \
+                      CCACHE_DIR=~/.ccache \
+                      LD=ld.lld
+
 
 cp out/arch/arm64/boot/Image.$COMPRESSION-dtb _anykernel/
 cp out/arch/arm64/boot/dtbo.img _anykernel/
